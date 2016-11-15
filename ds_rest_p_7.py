586de0602153e821232ff83d79d9beb6b0e5b172
김상미 <nsmi1229@naver.com>
2016-11-15 19:13:58 +0900

    Add files via upload

---
+++ ds_rest_p_7.py
@@ -0,0 +1,38 @@
+
+def getKey(keyPath):
+    d=dict()
+    f=open(keyPath,'r')
+    for line in f.readlines():
+        row=line.split('=')
+        row0=row[0]
+        d[row0]=row[1].strip()
+    return d
+
+import os
+keyPath=os.path.join(os.getcwd(), 'src', 'key.properties')
+key=getKey(keyPath)
+
+import os
+import requests
+_url='http://openAPI.seoul.go.kr:8088'
+_key=str(key['dataseoul'])
+_type='xml'
+_service='CardSubwayStatisticsService'
+_start_index=1
+_end_index=5
+_use_mon='201306'
+_maxIter=2
+_iter=0
+
+while _iter<_maxIter:
+    _api=os.path.join(_url,_key,_type,_service,str(_start_index),str(_end_index),_use_mon)
+    #print _api
+    response = requests.get(_api).text
+    print response
+    _start_index+=5
+    _end_index+=5
+    _iter+=1
+    
+use ds_rest_subwayPassengers_mongo_db
+db.db_rest_subway.find({"CardSubwayStatisticsService.row.SUB_STA_NM":"강남구청"})
+            
